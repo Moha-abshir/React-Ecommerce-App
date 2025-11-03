76bce7c7a2b1031ca5823b75162bf2087d1d3b59
Mohammed Abshir <moabshir06@gmail.com>
2025-11-03 13:17:33 +0300

    FInished styling the homepage

--- Ecommerce-Website/src/Components/HomePage.jsx
+++ Ecommerce-Website/src/Components/HomePage.jsx
@@ -1,9 +1,44 @@
 import { Nav } from './Nav'
+import { FaShoppingCart } from 'react-icons/fa';
 import './homepage.css'
 
 export function HomePage(){
 
     return(
-        <Nav/>
+        <>
+            <Nav/>
+
+            <main>
+                <section className="homepage-hero">
+      
+                    <div className="hero-text">
+                        
+                        <h1 className="hero-headline">Why Pay More for Elegance? The Sale You’ve Been Waiting For.</h1> 
+                        <p className="hero-subheadline">
+                        Our biggest price drops of the season are here!! Luxury items at prices you’ll hardly believe. Don’t wait... these deals vanish fast.
+                        </p>
+                        
+                        <div className="CTA">
+                            <button className="shop cta-btn">
+                                <FaShoppingCart style={{ marginRight: '8px' }} /> 
+                                SHOP NOW
+                            </button>
+                            <button className='cta-btn learn'> Learn More</button>
+                        </div>
+                    </div>
+
+                </section>
+
+                <section className="footer">
+                    <div className="provider">
+                        <span>Data Provided by </span> <a className='provide' href="https://fakeapi.platzi.com">Platzi Fake Store API</a>
+                    </div>
+                    <div className="contact">
+                        <p> &copy; 2025 | <a className='git' href="https://github.com/Moha-abshir">Moha Abshir</a></p>
+                    </div>
+                </section>
+            </main>
+
+        </>
     )
 }
