bab53abb90de121c1d366c0c90e5e0f67800fa32
Mohammed Abshir <moabshir06@gmail.com>
2025-11-05 14:44:23 +0300

    Made sure the product page displayes all of the images that was fetched from the API

--- Ecommerce-Website/src/Components/ProductGrid.jsx
+++ Ecommerce-Website/src/Components/ProductGrid.jsx
@@ -1,30 +1,43 @@
-import sunglasses from '../assets/Sunglasses.jpeg';
+import axios from 'axios';
 import './productgrid.css'
+import { useEffect, useState } from 'react';
 export function ProductGrid(){
+    const [products, setProducts] = useState([]);
+    
+    useEffect(()=>{
+        const getProducts = async function () {
+            const res = await axios.get('https://api.escuelajs.co/api/v1/products');
+            setProducts(res.data);
+        }
+        getProducts();
+    }, []);
+
+    console.log(products[0])
     return(
         <div className="product-grid">
-            <div className='product'>
-                <div className='image-container'>
-                    <img src={sunglasses} alt="sunglasses" />
-                </div>
-                <div className="product-details">
-                    <div className="product-name">
-                        <p>Trendy Pink-Tinted Sunglasses</p>
+
+            {products.map((product)=>{
+                return(
+                    <div className='product' key={product.id}>
+                        <div className='image-container'>
+                            <img src={`${product.images[0]}`} alt={product.title}/>
+                        </div>
+                        <div className="product-details">
+                            <div className="product-name">
+                                <p>{product.title}</p>
+                            </div>
+                            <div className="product-price">
+                                <p>Price: £${product.price}</p>
+                            </div>
+                        </div>
+                        <div className="product-more-cta">
+                            <button className='more'>More Detail</button>
+                            <button className='cta'>Add To Cart</button>
+                        </div>
                     </div>
-                    <div className="product-price">
-                        <p>Price: £38</p>
-                    </div>
-                </div>
-                <div className="product-more-cta">
-                    <button className='more'>More Detail</button>
-                    <button className='cta'>Add To Cart</button>
-                </div>
-            </div>
-            <div className='product'></div>
-            <div className='product'></div>
-            <div className='product'></div>
-            <div className='product'></div>
-            <div className='product'></div>
+                )
+            })}
+            
         </div>
     )
 }
