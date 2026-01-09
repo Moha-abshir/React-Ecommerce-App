8779b8ca2a16ad71bbe088d3b409e026473ab57c
Mohammed Abshir <moabshir06@gmail.com>
2026-01-09 07:46:14 +0300

    Changed the prouct file to tsx.

--- Ecommerce-Website/src/types/type.ts
+++ Ecommerce-Website/src/types/type.ts
@@ -1,9 +1,9 @@
-//When an interface is shared across multiple components, just place the shared interface here and import it in the
-//components that need to use it.
+//When an interface is shared across multiple components, just place the shared interface here and export 
+//Import it in the components that need to use it.
 
 export interface Product { //We export the interface so that it can be accessed by other files
     id: number,
     title: string,
     price: number, 
     images: string[];
-};
+}; 
