7e3dea7d6eac072968a3e26afd042f8254b30dbf
Mohammed Abshir <moabshir06@gmail.com>
2026-01-11 08:36:52 +0300

    Changed the .jsx to tsx

---
+++ Ecommerce-Website/src/Components/products/ProductDetail.tsx
@@ -0,0 +1,38 @@
+import { useParams, Link } from "react-router-dom";
+import { Product } from "../../types/type";
+import { Nav } from "../nav/Nav";
+import { GiReturnArrow } from "react-icons/gi";
+import './productdetail.css'
+interface ProductDetailProps{
+    products: Product[],
+    addToCart: (id: number)=> void
+}
+
+export function ProductDetail({products, addToCart}: ProductDetailProps){
+    const {id} = useParams();
+    const product = products.find(p=> Number(p.id)===Number(id));
+
+    return(
+        <>
+            <Nav/>
+            <div className="more-information">
+                <div className="image-container">
+                    <img src={`${product.images[0]}`} alt={product.title}/>
+                </div>
+                <div className="detail">
+                    <div className="name">{product.title}</div>
+                    <div className="category ">Category: {product.category.name}</div>
+                    <div className="price">Price: ${product.price}</div>
+                </div>
+                <div className="description">
+                    <h4>Description</h4>
+                    <p>{product.description}</p>
+                </div>
+                <div className="navigate">
+                    <Link to='/products'><button className="return"><GiReturnArrow size={40} color="white"/></button></Link>
+                    <Link to='cart'><button className="add" onClick={()=>{addToCart(product.id)}}>Add To Cart</button></Link>
+                </div>
+            </div>
+        </>
+    )
+}
